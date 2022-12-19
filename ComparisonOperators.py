123f2a0d241b978c75e38b6ff0503ab7e7157706
AkshayPak <120776903+AkshayPak@users.noreply.github.com>
2022-12-19 14:06:47 +0530

    ComparisonOperators

---
+++ ComparisonOperators.py
@@ -0,0 +1,17 @@
+# Defining variable c and b
+c = 15
+b = 16
+if c>b:
+    print("c is greater than b")
+if c<b :
+    print("c is less than b")
+if c>=b :
+    print("c is greater than or equal to b")
+if c<=b :
+    print("c is less than or eqaul to b")
+if c!=b :
+    print("c is not equal to b")
+if c==b :
+    print("c is eqaul to b")
+    
+    
