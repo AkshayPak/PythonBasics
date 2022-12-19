76a3c1789c59dabffbe2fc154ccf9f9fa5153b7e
AkshayPak <120776903+AkshayPak@users.noreply.github.com>
2022-12-19 11:24:20 +0530

    FirstIssueSolution

---
+++ assignmentOperatorsIssue.py
@@ -0,0 +1,20 @@
+# Initializing a variable named "a" with any integer value
+
+a = 100
+print(a)
+a += 10
+print(a)
+a -= 10
+print(a)
+a *= 10
+print(a)
+a /= 10
+print(a)
+a **= 10
+print(a)
+a //= 10
+print(a)
+
+
+
+
